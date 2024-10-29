b9814caf4c11585745c12121d438adf9e21a5728
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-29 17:12:04 +0330

    Main module connect event module

---
+++ Src/Applications/Evently.Application/Models/WeatherForecast.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Evently.Application.Models;
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
--- Src/Applications/Evently.Application/Program.cs
+++ Src/Applications/Evently.Application/Program.cs
@@ -1,44 +1,75 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+using System.Linq;
+using Evently.Application.Models;
+using Evently.Modules.Events.Application;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+namespace Evently.Application;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddEventModule(builder.Configuration);
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.ApplyEventMigration();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapEventEndpoints();
+
+        string[] summaries = new[]
+        {
+            "Freezing", 
+            "Bracing", 
+            "Chilly", 
+            "Cool", 
+            "Mild", 
+            "Warm", 
+            "Balmy", 
+            "Hot", 
+            "Sweltering", 
+            "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index => {
+                return new WeatherForecast
+                (
+                    DateOnly.FromDateTime(
+                        DateTime.Now.AddDays(index)
+                    ),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                );
+            })
+            .ToArray();
+            return forecast;
+        })
+            .WithName("GetWeatherForecast");
+
+        app.Run();
+
+    }
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
