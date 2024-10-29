ad8f5f549ff0cea60a2bef77f330257d601eb15e
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-29 17:11:34 +0330

    Create some extension method for isolation data and let main module to connected

---
+++ Src/Modules/Events/Evently.Modules.Events.Application/EventsModule.cs
@@ -0,0 +1,41 @@
+﻿using Evently.Commons.Application.Extensions;
+using Evently.Modules.Events.Application.Events.Endpoints.Create;
+using Evently.Modules.Events.Application.Events.Endpoints.Get;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Evently.Modules.Events.Application;
+
+public static class EventsModule
+{
+    public static IServiceCollection AddEventModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        string databaseConnectionString = configuration.GetConnectionString("SqliteDefault")!;
+
+        services.AddDbContext<Database.EventsDatabaseContext>(options => 
+            options.UseSqlite(
+                databaseConnectionString,
+                sqliteOptions => sqliteOptions
+                    .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Database.Schemas.Event)
+            )
+            .UseSnakeCaseNamingConvention()
+        );
+
+        return services;
+    }
+
+    public static void MapEventEndpoints(this IEndpointRouteBuilder app)
+    {
+        CreateEvent.MapEndpoint(app);
+        GetEvent.MapEndpoint(app);
+    }
+
+    public static void ApplyEventMigration(this IApplicationBuilder app)
+    {
+        app.ApplyMigrations<Database.EventsDatabaseContext>();
+    }
+}
---
+++ Src/Modules/Events/Evently.Modules.Events.Application/Tags.cs
@@ -0,0 +1,6 @@
+﻿namespace Evently.Modules.Events.Application;
+
+internal static class Tags
+{
+    internal const string Events = "Events";
+}
