9fccc904b604fffe88021091e58176fc95f9cba5
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-30 17:24:52 +0330

    Create implements the interfaces base on dbcontext concept

---
+++ Src/Commons/Evently.Commons.Events.Infrastructure/Abstractions/DatabaseContextRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Evently.Commons.Application.Contracts;
+using Evently.Commons.Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Commons.Infrastructure.Abstractions;
+
+public abstract class DatabaseContextRepository<TDbContext, TEntity, TKey> : IRepository<TEntity, TKey> 
+    where TEntity : class, IEntity<TKey>
+    where TDbContext : DbContext
+{
+    protected TDbContext Context { get; }
+
+    protected DbSet<TEntity> Set { get; }
+
+    public DatabaseContextRepository(TDbContext context)
+    {
+        Context = context;
+
+        Set = context.Set<TEntity>();
+    }
+
+    public Task<bool> ExistAsync(TKey id, CancellationToken cancellation)
+    {
+        return ExistAsync(model => model.Id!.Equals(id), cancellation);
+    }
+
+    public Task<bool> ExistAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellation)
+    {
+        return Query().AnyAsync(predicate, cancellation);
+    }
+
+    public async Task InsertAsync(TEntity entity, CancellationToken cancellation)
+    {
+        await Set.AddAsync(entity, cancellation);
+    }
+
+    public IQueryable<TEntity> Query()
+    {
+        return Set;
+    }
+}
---
+++ Src/Commons/Evently.Commons.Events.Infrastructure/Abstractions/DatabaseContextUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Evently.Commons.Application.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Commons.Infrastructure.Abstractions;
+
+public abstract class DatabaseContextUnitOfWork<TDbContext> : IUnitOfWork where TDbContext : DbContext
+{
+    private readonly TDbContext _dbContext;
+
+    public DatabaseContextUnitOfWork(TDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellation)
+    {
+        return _dbContext.SaveChangesAsync(cancellation);
+    }
+}
+
