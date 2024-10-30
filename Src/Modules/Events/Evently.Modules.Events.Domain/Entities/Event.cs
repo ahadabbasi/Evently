5d188a841a1ca50dedd707fbebdd213399fdbec2
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-30 17:25:34 +0330

    Refactoring event modules base on clean and implement interfaces

---
+++ Src/Modules/Events/Evently.Modules.Events.Domain/Entities/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Evently.Commons.Domain.Contracts;
+
+namespace Evently.Modules.Events.Domain.Entities;
+
+public sealed class Event : IEntity<Guid>
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string Location { get; set; }
+
+    public DateTime StartAtUtc { get; set; }
+
+    public DateTime EndsAtUtc { get; set; }
+
+    public EventStatus Status { get; set; }
+}
