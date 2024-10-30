5b8558209215d3415557f93aa359148d91329ebc
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-30 17:24:20 +0330

    Create interface for unit of work and repository

---
+++ Src/Commons/Evently.Commons.Events.Application/Contracts/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Evently.Commons.Domain.Contracts;
+
+namespace Evently.Commons.Application.Contracts;
+
+public interface IRepository<TEntity, TKey> where TEntity : class, IEntity<TKey>
+{
+    IQueryable<TEntity> Query();
+
+    Task InsertAsync(TEntity entity, CancellationToken cancellation);
+
+    Task<bool> ExistAsync(TKey id, CancellationToken cancellation);
+
+    Task<bool> ExistAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellation);
+}
