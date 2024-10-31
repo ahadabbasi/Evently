36c1a0e0602a99b218f9c55abfc5ed40a4b23ebc
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-31 12:55:47 +0330

    Trying to implement result pattern

---
+++ Src/Commons/Evently.Commons.Events.Domain/Abstractions/Result/Error.cs
@@ -0,0 +1,29 @@
+﻿namespace Evently.Commons.Domain.Abstractions.Result;
+
+public record Error
+{
+    public static readonly Error None = string.Empty;
+
+    public static readonly Error NullValue = "General.Null";
+
+    public Error(string code) : this(code, null)
+    { 
+    }
+
+    public Error(string code, string? description)
+    {
+        Code = code;
+
+        Description = description;
+    }
+
+    public string Code { get; }
+
+    public string? Description { get; }
+
+    public static Error NotFound<TKey>(string code, TKey value) => (code, string.Format("The entity with the identifier `{0}` was not found", value));
+
+    public static implicit operator Error(string code) => (code, null);
+
+    public static implicit operator Error((string code, string? description) value) => new(value.code, value.description);
+}
---
+++ Src/Commons/Evently.Commons.Events.Domain/Abstractions/Result/Result.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Evently.Commons.Domain.Abstractions.Result;
+
+public class Result
+{
+    protected Result(bool success, IEnumerable<Error> errors)
+    {
+        if(success && errors.Any() || !success && !errors.Any())
+        {
+            throw new Exception();
+        }
+
+        IsSuccess = success;
+
+        Errors = errors;
+    }
+
+    public bool IsSuccess { get; protected set; }
+
+    public IEnumerable<Error> Errors { get; protected set; }
+
+    public static Result Success() => new Result(true, new Error[] { });
+
+    public static Result<TValue> Success<TValue>(TValue value) => new Result<TValue>(value, true, new Error[] { });
+
+    public static Result Failure(IEnumerable<Error> errors) => new Result(false, errors);
+
+    public static Result<TValue> Failure<TValue>(IEnumerable<Error> errors) => new Result<TValue>(default, true, errors);
+
+    public static implicit operator Result(Error error) => Failure(new Error[] { error });
+
+    public static implicit operator Result(Error[] errors) => Failure(errors);
+}
+
+public class Result<TValue> : Result
+{
+    internal Result(TValue? value, bool success, IEnumerable<Error> errors) : base(success, errors)
+    {
+        if(success && value == null || !success && value != null)
+        {
+            throw new Exception();
+        }
+
+        Value = value!;
+    }
+
+    public TValue Value { get; protected set; }
+
+    public static implicit operator Result<TValue>(TValue? value) =>
+        value != null ? Success(value) : Failure<TValue>(new Error[] { Error.NullValue });
+
+    public static implicit operator Result<TValue>(Error error) => Failure<TValue>(new Error[] { error });
+
+    public static implicit operator Result<TValue>(Error[] errors) => Failure<TValue>(errors);
+}
