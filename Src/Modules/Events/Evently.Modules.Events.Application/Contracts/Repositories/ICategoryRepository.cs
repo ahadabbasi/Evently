54d77c245e5ef7605f40182746df7d13e393ff03
Ahad Abbasi <ahadabbasi@gmail.com>
2024-11-05 15:06:46 +0330

    Update the name to stop naming collision

--- Src/Modules/Events/Evently.Modules.Events.Application/Contracts/Repositories/ICategoryRepository.cs
+++ Src/Modules/Events/Evently.Modules.Events.Application/Contracts/Repositories/ICategoryRepository.cs
@@ -3,6 +3,6 @@ using Evently.Commons.Application.Contracts;
 using Evently.Modules.Events.Domain.Entities;
 
 namespace Evently.Modules.Events.Application.Contracts.Repositories;
-public interface ICategoryRepository : IRepository<Category, Guid>
+public interface ICategoryRepository : IRepository<Domain.Entities.Category, Guid>
 {
 }
