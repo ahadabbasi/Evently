00ead08f22d376171354087461d8bc4c3a971177
Ahad Abbasi <ahadabbasi@gmail.com>
2024-11-05 10:49:21 +0330

    Update interface to refuse collision of nameing two thing same

--- Src/Modules/Events/Evently.Modules.Events.Application/Contracts/Repositories/ITicketTypeRepository.cs
+++ Src/Modules/Events/Evently.Modules.Events.Application/Contracts/Repositories/ITicketTypeRepository.cs
@@ -6,7 +6,7 @@ using Evently.Modules.Events.Domain.Entities;
 
 namespace Evently.Modules.Events.Application.Contracts.Repositories;
 
-public interface ITicketTypeRepository : IRepository<TicketType, Guid>
+public interface ITicketTypeRepository : IRepository<Domain.Entities.TicketType, Guid>
 {
     Task<bool> ExistsAnyTiketForEventByIdAsync(Guid eventId, CancellationToken cancellation);
 }
