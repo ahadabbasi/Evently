3d669af579a8c8b6abbbe60359412c7526f54b41
Ahad Abbasi <ahadabbasi@gmail.com>
2024-11-05 10:47:33 +0330

    Trying to create interface for mapendpoint but was a failure

---
+++ Src/Commons/Evently.Commons.Events.Presentation/IMapEndpoint.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Commons.Presentation;
+
+
+public interface IMapEndpoint
+{
+    static void MapEndpoint(IEndpointRouteBuilder app) => throw new NotImplementedException();
+}
